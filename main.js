07d8c81efc1a0a6f7c380ac33db8bebde18a6c2b
mojotron <igormalj@gmail.com>
2021-02-13 20:23:52 +0100

    Add button and eventlistener

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 let Phrase = require("mojo-palindrome");
 
-let string = prompt("Please enter a string for palindrome testing: ");
-let phrase = new Phrase(string);
-
-if (phrase.palindrome()) {
-  alert(`"${phrase.content}" is a palindrome!`);
-} else {
-  alert(`"${phrase.content}" is not a palindrome.`);
+function palindromeTester() {
+  let string = prompt("Please enter a string for palindrome testing:");
+  let phrase = new Phrase(string);
+  let palindromeResult = document.querySelector("#palindromeResult");
+  if (phrase.palindrome()) {
+    palindromeResult.innerHTML = `"${phrase.content}" is a palindrome!`;
+  } else {
+    palindromeResult.innerHTML = `"${phrase.content}" is not a palindrome.`;
+  }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  let form = document.querySelector("#palindromeTester");
+  form.addEventListener("click", function() {
+    palindromeTester();
+  });
+});
+
+
